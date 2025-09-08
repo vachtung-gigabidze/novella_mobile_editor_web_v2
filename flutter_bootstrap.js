6554a9e53a4d3772c1db02b0a3251d24bef72148
dimitrii <dmitry.zyadik@gmail.com>
2025-09-08 09:57:21 +0500

    update web

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"ddf47dd3ff96dbde6d9c614db0d7f019d7c7a2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4132331339"
+    serviceWorkerVersion: "517512943"
   }
 });
